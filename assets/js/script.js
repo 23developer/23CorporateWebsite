d815a5a62be630c22a5bfdc668c38d1ed4b1fb3d
Shunichi Sakai <jmjm_jmjm@me.com>
2020-10-24 13:13:48 +0900

    下からのスクロール時と画面更新時にもフェードイン処理をするように変更

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+
     //navにマウスオーバー時に文字色変更
     $('.nav li a').each(function() {
         if($(this).hasClass("not-frame-nav")){
@@ -16,24 +17,25 @@ $(document).ready(function(){
                     $(this).stop().animate({'background-color': '#F8DD00', 'color': '#FFF', 'border-color': '#111'},200);
                 },
                 function(){
-                    $(this).stop().animate({'background-color': '#FFF', 'color': '#111', 'border-color': '#111'},200);
+                    $(this).stop().animate({'background-color': 'transparent', 'color': '#111', 'border-color': '#111'},200);
                 }
         )};
     });
 
 
+    // フェードイン
+    $('.section_title').addClass("scrollanime slide-left") // セクションタイトルを左からフェードイン
+    $('.bold_sentence').addClass("scrollanime") // bold-sentenceクラスをフェードイン
 
-    //フェードイン
-    $('.section_title').addClass("scrollanime slide-left") //セクションタイトルをフェードイン要素に変更
-
+    // 画面スクロール時
     $(window).scroll(function () {
         const wHeight = $(window).height();
         const scrollAmount = $(window).scrollTop();
-        $('.scrollanime').each(function () { //scrollanimeクラス要素にフェードインを追加
+        $('.scrollanime').each(function () { // scrollanimeクラス要素にフェードインを追加
             const targetPosition = $(this).offset().top;
-            if(scrollAmount > targetPosition - wHeight + 100) {
+            if(scrollAmount + wHeight - 100 > targetPosition && targetPosition > scrollAmount) { // 要素が画面内にあるか判定
                 if($(this).hasClass("slow-fadein")){
-                    $(this).addClass("slowFadein") //slow-fadeinクラスなら遅い変化
+                    $(this).addClass("slowFadein") // slow-fadeinクラスなら遅い変化
                 } else {
                     $(this).addClass("fadeInDown")
                 };
@@ -41,5 +43,20 @@ $(document).ready(function(){
         });
     });
 
+    // リロード時(画面ストローク時と同じ処理だからできればまとめたい)
+    $(window).on("load",function () {
+        const wHeight = $(window).height();
+        const scrollAmount = $(window).scrollTop();
+        $('.scrollanime').each(function () { // scrollanimeクラス要素にフェードインを追加
+            const targetPosition = $(this).offset().top;
+            if(scrollAmount + wHeight - 100 > targetPosition && targetPosition > scrollAmount) { // 要素が画面内にあるか判定
+                if($(this).hasClass("slow-fadein")){
+                    $(this).addClass("slowFadein") // slow-fadeinクラスなら遅い変化
+                } else {
+                    $(this).addClass("fadeInDown")
+                };
+            };
+        });
+    });
 
 });
