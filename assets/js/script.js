178a86599bbd874420bead748cf6657e76e47253
Shunichi Sakai <jmjm_jmjm@me.com>
2020-10-25 18:42:47 +0900

    ajaxによるgoogleformeへのお問い合わせ内容送信実装

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,6 @@
 $(document).ready(function(){
-    // navにマウスオーバー時に文字色変更
-    $('#header .nav li a').each(function() {
-        if($(this).hasClass("not-frame-nav")){
-            $(this).hover(
-                function(){
-                    $(this).stop().animate({'color': '#F8DD00'},300);
-                },
-                function(){
-                    $(this).stop().animate({'color': '#010101'},300);
-                }
-            )
-        } else{
-            $(this).hover(
-                function(){
-                    $(this).stop().animate({'background-color': '#F8DD00', 'color': '#FFF', 'border-color': '#111'},200);
-                },
-                function(){
-                    $(this).stop().animate({'background-color': 'transparent', 'color': '#111', 'border-color': '#111'},200);
-                }
-        )};
-    });
-
-
+    
+    
     // membersにOther membersボタンを実装
     $('.see_more_button').on('click',function() {
         $('.see_more_button').addClass("hide_contents");
@@ -31,12 +10,12 @@ $(document).ready(function(){
             }
         });
     });
-
-
+    
+    
     // フェードイン
     $('.section_title').addClass("scrollanime slide-left") // セクションタイトルを左からフェードイン
     $('.bold_sentence').addClass("scrollanime") // bold-sentenceクラスをフェードイン
-
+    
     // 画面スクロール時
     $(window).scroll(function () {
         const wHeight = $(window).height();
@@ -52,7 +31,7 @@ $(document).ready(function(){
             };
         });
     });
-
+    
     // リロード時(画面ストローク時と同じ処理だからできればまとめたい)
     $(window).on("load",function () {
         const wHeight = $(window).height();
@@ -68,8 +47,8 @@ $(document).ready(function(){
             };
         });
     });
-
-
+    
+    
     // SNSアイコンにホバー時、準備中メッセージ表示
     $('#sns-setting-up-message').hide()
     $('.sns-icon').hover(
@@ -78,38 +57,62 @@ $(document).ready(function(){
         }, function() {
             $('#sns-setting-up-message').hide();
         }
-    );
-    
-
-    // スマホの時のみドロワーメニュー有効化
-    $('.drawer').drawer();
-    $('.drawer-menu li').on('click', function() {
-        $('.drawer').drawer('close');
-    });
-
-    
-    //formの内容をGoogleformへajax送信
-    $('#ajax').on('click',function(){
-        var name = document.forms.otoiawase.name.value;
-        var email = document.forms.otoiawase.email.value;
-        var msg = $('[name=entry.1369150218]').val();
-        window.open().location.href = '/thx.html';
+        );
         
-        $.ajax({
-            url: 'https://docs.google.com/forms/u/0/d/e/1FAIpQLScTYYAUyyrIaRm7lbWuf0p2ZtT6wxdRUiWQJ6Jfm-cgxPiPkA/formResponse',
-            type: 'POST',
-            data: {
-                "entry.698167586": name,
-                "entry.393581839": email,
-                "entry.1369150218": msg
-            },
-            dataType: "xml",    
-        }).done(function(data) {
-            alert("ok");
-            window.open().location.href = '/thx.html';
-        }).fail(function(XMLHttpRequest, textStatus, errorThorown) {
-            // alert("error");
-            window.open().location.href = '/thx.html';
-        })
+        
+        // スマホの時のみドロワーメニュー有効化
+        $('.drawer').drawer();
+        $('.drawer-menu li').on('click', function() {
+            $('.drawer').drawer('close');
+        });
+        
+        
+        //formの内容をGoogleformへajax送信
+        $("#googleforme").submit(function() {
+            var name = $("#name").val();
+            var email = $("#email").val();
+            var msg = $("#msg").val();
+
+            $.ajax({
+                url: "https://docs.google.com/forms/u/0/d/e/1FAIpQLScTYYAUyyrIaRm7lbWuf0p2ZtT6wxdRUiWQJ6Jfm-cgxPiPkA/formResponse",
+                data: {"entry.698167586": name, "entry.393581839": email, "entry.1369150218": msg},
+                type: "POST",
+                dtaatype: "xml",
+                statuscode: {
+                    0: function() {
+                        alert("送信が完了しました。");
+                        window.location().href="https://google.com";
+                    },
+                    200: function() {
+                        alert("error");
+                        window.location().href="https://google.com";
+                    }
+                }
+            });
+            event.preventDefault();
+        });
+
+
+        // navにマウスオーバー時に文字色変更
+        $('.hover-contents').each(function() {
+            if($(this).hasClass("not-frame-nav")){
+                $(this).hover(
+                    function(){
+                        $(this).stop().animate({'color': '#F8DD00'},300);
+                    },
+                    function(){
+                        $(this).stop().animate({'color': '#010101'},300);
+                    }
+                )
+            } else{
+                $(this).hover(
+                    function(){
+                        $(this).stop().animate({'background-color': '#F8DD00', 'color': '#FFF', 'border-color': '#111'},200);
+                    },
+                    function(){
+                        $(this).stop().animate({'background-color': 'transparent', 'color': '#111', 'border-color': '#111'},200);
+                    }
+            )};
+        });
     });
-});
+    
