b3d4d5aae95eb3612c15c2d4d7b94909b7df2bad
Shunichi Sakai <jmjm_jmjm@me.com>
2020-10-25 15:07:48 +0900

    wide,mediumの時のハンバーガーメニュー削除

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
-
-    //navにマウスオーバー時に文字色変更
-    $('.nav li a').each(function() {
+    // navにマウスオーバー時に文字色変更
+    $('#header .nav li a').each(function() {
         if($(this).hasClass("not-frame-nav")){
             $(this).hover(
                 function(){
@@ -20,6 +19,17 @@ $(document).ready(function(){
                     $(this).stop().animate({'background-color': 'transparent', 'color': '#111', 'border-color': '#111'},200);
                 }
         )};
+    });
+
+
+    // membersにOther membersボタンを実装
+    $('.see_more_button').on('click',function() {
+        $('.see_more_button').addClass("hide_contents");
+        $('.member').each(function() {
+            if($(this).hasClass("hide_contents")) {
+                $(this).removeClass("hide_contents");
+            }
+        });
     });
 
 
@@ -71,30 +81,35 @@ $(document).ready(function(){
     );
     
 
-    // ドロワーメニュー有効化
+    // スマホの時のみドロワーメニュー有効化
     $('.drawer').drawer();
     $('.drawer-menu li').on('click', function() {
         $('.drawer').drawer('close');
     });
 
-
-    // formの内容をGoogleformへajax送信
-    // $('#ajax').on('click',function(){
-    //     // var name = 
-
-    //     $.ajax({
-    //         url: 'https://docs.google.com/forms/u/0/d/e/1FAIpQLScTYYAUyyrIaRm7lbWuf0p2ZtT6wxdRUiWQJ6Jfm-cgxPiPkA/formResponse',
-    //         type: 'POST',
-    //         data: {
-    //             "entry.698167586": "asd",
-    //             "entry.393581839": "a@a",
-    //             "entry.1369150218": "as"
-    //         },
-    //         dataType: "xml",    
-    //     }).done(function(data) {
-    //         alert("ok");
-    //     }).fail(function(XMLHttpRequest, textStatus, errorThorown) {
-    //         alert("error");
-    //     })
-    // });
+    
+    //formの内容をGoogleformへajax送信
+    $('#ajax').on('click',function(){
+        var name = document.forms.otoiawase.name.value;
+        var email = document.forms.otoiawase.email.value;
+        var msg = $('[name=entry.1369150218]').val();
+        window.open().location.href = '/thx.html';
+        
+        $.ajax({
+            url: 'https://docs.google.com/forms/u/0/d/e/1FAIpQLScTYYAUyyrIaRm7lbWuf0p2ZtT6wxdRUiWQJ6Jfm-cgxPiPkA/formResponse',
+            type: 'POST',
+            data: {
+                "entry.698167586": name,
+                "entry.393581839": email,
+                "entry.1369150218": msg
+            },
+            dataType: "xml",    
+        }).done(function(data) {
+            alert("ok");
+            window.open().location.href = '/thx.html';
+        }).fail(function(XMLHttpRequest, textStatus, errorThorown) {
+            // alert("error");
+            window.open().location.href = '/thx.html';
+        })
+    });
 });
